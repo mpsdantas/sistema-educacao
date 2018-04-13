1ac79e0ede1453e644268945a26b1f119c87f914
Marcus Dantas <mpsdantas15@gmail.com>
2018-04-13 01:17:21 -0300

    feat(/src/views/): Enviando e validando formulário.

---
+++ src/controllers/autenticacao/cadastro.js
@@ -0,0 +1,3 @@
+exports.realizarCadastro = (application, req, res) => {
+    
+};
--- src/routes/autenticacao.js
+++ src/routes/autenticacao.js
@@ -2,4 +2,7 @@ module.exports = application => {
     application.get('/cadastro-usuario', (req, res) => { 
         res.render('autenticacao/cadastro-usuario')
     });
+    application.post('/cadastro-usuario', (req, res) => {
+        console.log(req.body);
+    });
 };
