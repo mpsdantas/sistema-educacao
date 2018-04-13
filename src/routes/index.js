7f42d9c5c0a8dd9b932dfe11c24de6a32be45113
Marcus Dantas <mpsdantas15@gmail.com>
2018-04-13 20:29:01 -0300

    feat(/scr/models/report):

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,27 @@
 const blockRouter = require('../controllers/blockRouter/index.js');
+const report = require('../controllers/report/index');
 module.exports = application => {
     application.get('/', (req,res) => {res.render('index')});
+    
     application.get('/report', blockRouter.statusUser, (req,res) => {
         res.render('report/form',{nome:req.session.nome});
     });
+
     application.get('/report/visualizar', blockRouter.statusUser, (req,res) => {
         res.render('report/visualizar',{nome:req.session.nome});
     });
-    application.get('/admin', blockRouter.statusAdmin, (req,res) => {res.render('admin/dashboard')});
+    
+    application.get('/admin', blockRouter.statusAdmin, (req, res) => { 
+
+        report.exibirDashboard(application, req, res);
+    });
     application.get('/admin/statistics', blockRouter.statusAdmin,  (req,res) => {
         res.render('admin/stats')
     });
+
     application.get('/admin/reports', blockRouter.statusAdmin, (req,res) => {res.render('admin/reports')});
 
     application.get('/perfil/user-perfil', blockRouter.statusUser, (req, res) => {
         res.render('perfil/user-perfil', {nome:req.session.nome})
-    })
+    });
 };
