ce2edd508365a260d01eb24ada67e5abe9a725f8
Rafael Garcia <rafaelgarcia2013@gmail.com>
2018-04-13 22:27:29 -0300

    feat(/admin/escolas): Módulo de Escolas em Admin

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,27 +1,40 @@
-const blockRouter = require('../controllers/blockRouter/index.js');
-const report = require('../controllers/report/index');
+const blockRouter = require("../controllers/blockRouter/index.js");
+const report = require("../controllers/report/index");
 module.exports = application => {
-    application.get('/', (req,res) => {res.render('index')});
-    
-    application.get('/report', blockRouter.statusUser, (req,res) => {
-        res.render('report/form',{nome:req.session.nome});
-    });
+  application.get("/", (req, res) => {
+    res.render("index");
+  });
 
-    application.get('/report/visualizar', blockRouter.statusUser, (req,res) => {
-        res.render('report/visualizar',{nome:req.session.nome});
-    });
-    
-    application.get('/admin', blockRouter.statusAdmin, (req, res) => { 
+  application.get("/report", blockRouter.statusUser, (req, res) => {
+    res.render("report/form", { nome: req.session.nome });
+  });
 
-        report.exibirDashboard(application, req, res);
-    });
-    application.get('/admin/statistics', blockRouter.statusAdmin,  (req,res) => {
-        res.render('admin/stats')
-    });
+  application.get("/report/visualizar", blockRouter.statusUser, (req, res) => {
+    res.render("report/visualizar", { nome: req.session.nome });
+  });
 
-    application.get('/admin/reports', blockRouter.statusAdmin, (req,res) => {res.render('admin/reports')});
+  application.get("/admin", blockRouter.statusAdmin, (req, res) => {
+    report.exibirDashboard(application, req, res);
+  });
+  application.get("/admin/statistics", blockRouter.statusAdmin, (req, res) => {
+    res.render("admin/stats");
+  });
+  application.get("/admin/escolas", blockRouter.statusAdmin, (req, res) => {
+    res.render("admin/escola/listar");
+  });
+  application.get(
+    "/admin/escolas/form",
+    blockRouter.statusAdmin,
+    (req, res) => {
+      res.render("admin/escola/form");
+    }
+  );
 
-    application.get('/perfil/user-perfil', blockRouter.statusUser, (req, res) => {
-        res.render('perfil/user-perfil', {nome:req.session.nome})
-    });
+  application.get("/admin/reports", blockRouter.statusAdmin, (req, res) => {
+    res.render("admin/reports");
+  });
+
+  application.get("/perfil/user-perfil", blockRouter.statusUser, (req, res) => {
+    res.render("perfil/user-perfil", { nome: req.session.nome });
+  });
 };
