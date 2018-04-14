6b3034d30f1f0638b2d121c23341a8474cd476d3
Rafael Garcia <rafaelgarcia2013@gmail.com>
2018-04-14 12:20:05 -0300

    Área de Admin

--- src/public/dashboard/js/stats-page.js
+++ src/public/dashboard/js/stats-page.js
@@ -1,5 +1,6 @@
 google.charts.load("current", { packages: ["corechart", "bar"] });
 google.charts.setOnLoadCallback(drawBasic);
+google.charts.setOnLoadCallback(drawBasicEscolas);
 google.charts.setOnLoadCallback(drawPieChart);
 google.charts.setOnLoadCallback(drawAxisTickColors);
 
@@ -41,6 +42,43 @@ function drawBasic() {
   chart.draw(data, options);
 }
 
+function drawBasicEscolas() {
+  var data = new google.visualization.DataTable();
+  data.addColumn("string", "Mês");
+  data.addColumn("number", "Número total de reports");
+
+  data.addRows([
+    ["Jan", 36],
+    ["Fev", 40],
+    ["Mar", 13],
+    ["Abr", 104],
+    ["Jun", 75],
+    ["Jul", 65],
+    ["Ago", 50],
+    ["Set", 3],
+    ["Out", 109],
+    ["Nov", 29],
+    ["Dez", 13]
+  ]);
+
+  var options = {
+    title: "Distribuição de Reports por mês",
+    hAxis: {
+      title: "Mês"
+    },
+    vAxis: {
+      title: "Quantidade de Reports"
+    },
+    colors: ["blue", "#004411"],
+    height: 400
+  };
+
+  var chart = new google.visualization.ColumnChart();
+  //document.getElementById("grafico-escolas")
+
+  chart.draw(data, options);
+}
+
 function drawPieChart() {
   var data = google.visualization.arrayToDataTable([
     ["Tipo de Report", "Quantidade"],
@@ -65,19 +103,19 @@ function drawPieChart() {
 }
 function drawAxisTickColors() {
   var data = google.visualization.arrayToDataTable([
-    ["City", "2010 Population", "2000 Population"],
-    ["New York City, NY", 8175000, 8008000],
-    ["Los Angeles, CA", 3792000, 3694000],
-    ["Chicago, IL", 2695000, 2896000],
-    ["Houston, TX", 2099000, 1953000],
-    ["Philadelphia, PA", 1526000, 1517000]
+    ["Escolas", "Sugestões", "Reclamações"],
+    ["E. E. João Henrique", 100, 125],
+    ["E. E. Barão", 80, 100],
+    ["E. E. de Parnamirim", 56, 100],
+    ["E. E. de Currais Novos", 50, 80],
+    ["E. E. Rafael", 50, 70]
   ]);
 
   var options = {
-    title: "Population of Largest U.S. Cities",
+    title: "Distribuição por Escolas",
     chartArea: { width: "50%", height: 300 },
     hAxis: {
-      title: "Total Population",
+      title: "Quantidade de Reports",
       minValue: 0,
       textStyle: {
         bold: true,
@@ -91,7 +129,7 @@ function drawAxisTickColors() {
       }
     },
     vAxis: {
-      title: "City",
+      title: "Escolas",
       textStyle: {
         fontSize: 14,
         bold: true,
@@ -104,7 +142,28 @@ function drawAxisTickColors() {
       }
     }
   };
-  var chart = new google.visualization.BarChart(
+
+  var data = google.visualization.arrayToDataTable([
+    ["Escolas", "Sugestões", "Reclamações", "Outros", { role: "annotation" }],
+    ["E. E. João Henrique", 28, 24, 30, ""],
+    ["E. E. Barão", 21, 22, 27, ""],
+    ["E. E. de Parnamirim", 25, 19, 10, ""],
+    ["E. E. de Currais Novos", 28, 13, 23, ""],
+    ["E. E. de São Vicente", 28, 25, 24, ""],
+    ["E. E. de Cerro Corá", 25, 13, 23, ""],
+    ["E. E. de Natal", 22, 12, 12, ""],
+    ["E. E. de Carnaúba dos Dantas", 28, 19, 15, ""]
+  ]);
+
+  var options = {
+    width: 600,
+    height: 400,
+    legend: { position: "top", maxLines: 3 },
+    bar: { groupWidth: "75%" },
+    isStacked: true
+  };
+
+  var chart = new google.visualization.ColumnChart(
     document.getElementById("grafico-escolas")
   );
   chart.draw(data, options);
